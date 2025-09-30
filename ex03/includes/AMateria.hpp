8a035ef0518b25c8ddb5e227681b024046840fd5
SuPuHe <mizin.a.s.2006@gmail.com>
2025-09-30 12:17:49 +0200

    create Amateria class

--- ex03/includes/AMateria.hpp
+++ ex03/includes/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:06:31 by omizin            #+#    #+#             */
-/*   Updated: 2025/09/29 17:11:46 by omizin           ###   ########.fr       */
+/*   Updated: 2025/09/30 12:05:25 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@
 class AMateria
 {
 	protected:
-		;
+		std::string	_type;
 	public:
 		AMateria(std::string const & type);
+		AMateria(const AMateria &copy);
+		AMateria &operator=(const AMateria &copy);
+		virtual ~AMateria();
+		
 		std::string const & getType() const;
 		virtual AMateria* clone() const = 0;
 		virtual void use(ICharacter& target);
--- ex03/srcs/AMateria.cpp
+++ ex03/srcs/AMateria.cpp
@@ -6,7 +6,38 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:06:29 by omizin            #+#    #+#             */
-/*   Updated: 2025/09/29 17:06:30 by omizin           ###   ########.fr       */
+/*   Updated: 2025/09/30 12:17:08 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "main.hpp"
+
+AMateria::AMateria(std::string const & type) : _type(type)
+{
+	std::cout << "Amateria constructow with type: " << _type << std::endl;
+}
+
+AMateria::AMateria(const AMateria &copy) : _type(copy._type)
+{
+	std::cout << "Amateria copy constructor with type: " << _type << std::endl;
+}
+
+AMateria &AMateria::operator=(const AMateria &copy)
+{
+	if (this != &copy)
+		_type = copy._type;
+	std::cout << "Amateria copy assigment operator with type: " << _type << std::endl;
+	return *this;
+}
+
+AMateria::~AMateria()
+{
+	std::cout << "Amateria desructor with type: " << _type << std::endl;
+}
+
+std::string const & AMateria::getType() const { return _type; }
+
+void AMateria::use(ICharacter& target)
+{
+	std::cout << "Use some kind of undefined materia on " << target.getName() << std::endl;
+}
