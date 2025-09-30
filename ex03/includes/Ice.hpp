6eb1e31887f7a52b04b34f9d7e0ebb91b94060c4
SuPuHe <mizin.a.s.2006@gmail.com>
2025-09-30 14:13:02 +0200

    finish ice class

--- ex03/includes/Ice.hpp
+++ ex03/includes/Ice.hpp
@@ -6,8 +6,20 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:06:32 by omizin            #+#    #+#             */
-/*   Updated: 2025/09/29 17:07:39 by omizin           ###   ########.fr       */
+/*   Updated: 2025/09/30 14:02:14 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+class Ice: public AMateria
+{
+	public:
+		Ice();
+		Ice(const Ice &copy);
+		Ice &operator=(const Ice &copy);
+		~Ice();
+
+		AMateria* clone() const;
+		void use(ICharacter& target);
+};
--- ex03/srcs/Ice.cpp
+++ ex03/srcs/Ice.cpp
@@ -6,7 +6,38 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:06:27 by omizin            #+#    #+#             */
-/*   Updated: 2025/09/29 17:06:28 by omizin           ###   ########.fr       */
+/*   Updated: 2025/09/30 14:12:38 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "main.hpp"
+
+Ice::Ice(): AMateria("ice")
+{
+	std::cout << "Ice constructor called" << std::endl;
+}
+
+Ice::Ice(const Ice &copy): AMateria(copy)
+{
+	std::cout << "Ice copy constructor called" << std::endl;
+}
+
+Ice &Ice::operator=(const Ice &copy)
+{
+	if (this != &copy)
+		AMateria::operator=(copy);
+	std::cout << "Ice copy assigment operator called" << std::endl;
+	return *this;
+}
+
+Ice::~Ice()
+{
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+AMateria* Ice::clone() const { return new Ice(*this); }
+
+void Ice::use(ICharacter& target)
+{
+	std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
+}
