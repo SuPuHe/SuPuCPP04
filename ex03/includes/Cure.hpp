0173d9639fb09d3bb5f51bc2db2035701a56fdc5
SuPuHe <mizin.a.s.2006@gmail.com>
2025-09-30 14:11:57 +0200

    finish cure class

--- ex03/includes/Cure.hpp
+++ ex03/includes/Cure.hpp
@@ -6,8 +6,22 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:06:31 by omizin            #+#    #+#             */
-/*   Updated: 2025/09/29 17:07:40 by omizin           ###   ########.fr       */
+/*   Updated: 2025/09/30 14:01:46 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include "main.hpp"
+
+class Cure: public AMateria
+{
+	public:
+		Cure();
+		Cure(const Cure &copy);
+		Cure &operator=(const Cure &copy);
+		~Cure();
+
+		AMateria* clone() const;
+		void use(ICharacter& target);
+};
--- ex03/srcs/Cure.cpp
+++ ex03/srcs/Cure.cpp
@@ -6,7 +6,38 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:06:28 by omizin            #+#    #+#             */
-/*   Updated: 2025/09/29 17:06:29 by omizin           ###   ########.fr       */
+/*   Updated: 2025/09/30 14:11:10 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "main.hpp"
+
+Cure::Cure(): AMateria("cure")
+{
+	std::cout << "Cure constructor called" << std::endl;
+}
+
+Cure::Cure(const Cure &copy): AMateria(copy)
+{
+	std::cout << "Cure copy constructor called" << std::endl;
+}
+
+Cure &Cure::operator=(const Cure &copy)
+{
+	if (this != &copy)
+		AMateria::operator=(copy);
+	std::cout << "Cure copy assigment operator called" << std::endl;
+	return *this;
+}
+
+Cure::~Cure()
+{
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+AMateria* Cure::clone() const { return new Cure(*this); }
+
+void Cure::use(ICharacter& target)
+{
+	std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
+}
